8ab917915e4c806e3e7836bb4dcc078cbe3db7d0
zoetyande <zoetyandemohamed@yahoo.com>
2024-02-18 21:02:17 +0530

    Week 1 activities on variables, loops, and conditionals

---
+++ Meta FrontEnd/Course 2-Programming with javascript/Week 1/Declaring variable.js
@@ -0,0 +1,46 @@
+// Task 1
+var petDog = 'Rex';
+
+// Task 2
+var petCat = 'Pepper';
+
+// Task 3
+var petDog = 'Rex';
+console.log(petDog);
+
+// Task 4
+var petCat = 'Pepper';
+console.log(petCat);
+
+// Task 5
+var petDog = 'Rex';
+console.log("My pet dog's name is: " + petDog);
+
+// Task 6
+var petCat = 'Pepper';
+console.log("My pet cat's name is: " + petCat);
+
+// Task 7
+var catSound = "purr";
+
+// Task 8
+var dogSound = "woof";
+
+// Task 9
+var petDog = 'Rex';
+var dogSound = "woof";
+console.log(petDog, "says", dogSound);
+
+// Task 10
+var petCat = 'Pepper';
+var catSound = "purr";
+console.log(petCat, "says", catSound);
+
+// Task 11
+var catSound = "purr";
+catSound = "meow";
+
+// Task 12
+var petCat = 'Pepper';
+var catSound = "meow";
+console.log(petCat, "now says", catSound);
