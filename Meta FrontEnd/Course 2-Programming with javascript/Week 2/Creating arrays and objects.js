deea0e5c954886c80780004979c1f9eaf5574f97
zoetyande <zoetyandemohamed@yahoo.com>
2024-02-18 23:41:05 +0530

    Week 2 arrays, objects and functions

---
+++ Meta FrontEnd/Course 2-Programming with javascript/Week 2/Creating arrays and objects.js
@@ -0,0 +1,30 @@
+// Task 1
+var clothes = [];
+
+// Task 2
+clothes.push("hoodie");
+clothes.push('yellow t-shirt');
+clothes.push('slippers');
+clothes.push('new jeans');
+clothes.push("blue t-shirt");
+
+// Task 3
+clothes.pop();
+
+// Task 4
+clothes.push("black hoodie");
+
+// Task 5
+console.log(clothes[2]);
+
+// Task 6
+var favCar = {};
+
+// Task 7
+favCar.color = "red";
+
+// Task 8
+favCar.convertible = true;
+
+// Task 9
+console.log(favCar);
