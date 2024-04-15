697044f435377e9947d9a155b1a9545ca270258e
zoetyande <zoetyandemohamed@yahoo.com>
2024-04-15 07:13:10 +0530

    Testing in Js using jest

---
+++ Meta FrontEnd/Course 2-Programming with javascript/Week 4/addFive.js
@@ -0,0 +1,18 @@
+// Installing node:
+// Go on  and then download and then install nodeJs
+
+// https://nodejs.org
+
+
+// node --version
+
+// npm --version
+
+function addFive(val){
+    return val + 5;
+};
+
+module.exports = addFive;
+
+// in package.json change under scripts test: "jest"
+// npm run test
