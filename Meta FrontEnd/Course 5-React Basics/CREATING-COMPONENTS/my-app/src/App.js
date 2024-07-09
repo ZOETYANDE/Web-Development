918eecd867709ebe20fd4c330f5d6514b2eff816
zoetyande <zoetyandemohamed@yahoo.com>
2024-07-09 13:52:32 +0530

    Creating my first react Component

---
+++ Meta FrontEnd/Course 5-React Basics/CREATING-COMPONENTS/my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function Heading(){
+  return(
+    <div>
+      <h1>This is an h1 heading.</h1>
+    </div>
+    
+  )
+}
+
+function App() {
+  return (
+    <div className='App'>
+      This is the starting code for "Your first componenet" Ungraded lab
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
