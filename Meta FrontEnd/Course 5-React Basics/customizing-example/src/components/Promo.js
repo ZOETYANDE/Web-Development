28a123b7dd2146a3065fd43ac1eadbb97705f0b2
zoetyande <zoetyandemohamed@yahoo.com>
2024-07-09 14:44:33 +0530

    Customizing an react app project example

---
+++ Meta FrontEnd/Course 5-React Basics/customizing-example/src/components/Promo.js
@@ -0,0 +1,16 @@
+
+
+function Promo() {
+    return (
+        <div className="promo-section">
+            <div>
+                <h1>Don't miss this deal!</h1>
+            </div>
+            <div>
+                <h2>Suscribe to my newsletter and get all the shop times at 50% off!</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Promo;
